b73fd54abd2a88ec430ad47b9e5e1617f2b2dad6
Hobr <mail@hobr.site>
2026-09-03 00:10:18 +0800

    flake: cl-nix-lite init

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flakelight.url = "github:nix-community/flakelight";
+    cl-nix-lite.url = "github:hraban/cl-nix-lite/v0";
   };
 
   outputs =
@@ -9,9 +10,19 @@
     flakelight ./. {
       inherit inputs;
 
+      withOverlays = [ inputs.cl-nix-lite.overlays.default ];
+
       devShell =
-        pkgs: with pkgs; rec {
-          packages = [
+        pkgs:
+        with pkgs.lispPackagesLite;
+        lispDerivation {
+          name = "sunmao";
+          src = pkgs.lib.cleanSource ./.;
+          lispSystem = "dev";
+          lispDependencies = [ ];
+
+          dontBuild = true;
+          nativeBuildInputs = with pkgs; [
             pkg-config
             pre-commit
           ];
