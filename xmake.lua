225fa946803e89b094460e8fcbaff60830a5bb2a
Hobr <mail@hobr.site>
2026-09-10 00:10:58 +0800

    xmake: libyansuan static

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,6 @@ set_version("0.1.0")
 set_languages("c++latest", "c11")
 
 add_rules("mode.debug", "mode.release")
-set_warnings("all", "error")
 
 -- debug
 if is_mode("debug") then
@@ -12,6 +11,7 @@ if is_mode("debug") then
     set_optimize("none")
 else
     set_symbols("hidden")
+    set_warnings("all", "error")
     set_optimize("fastest")
     set_strip("all")
 end
@@ -24,15 +24,21 @@ function add_platform_specific_flags()
     end
 end
 
--- libyansuan
-target("libyansuan")
+-- libyansuan static
+target("libyansuan-static")
     set_kind("static")
+    add_files("src/foo.cpp")
+    add_platform_specific_flags()
+
+-- libyansuan shared
+target("libyansuan")
+    set_kind("shared")
     add_files("src/foo.cpp")
     add_platform_specific_flags()
 
 -- yansuan
 target("Yansuan")
     set_kind("binary")
-    add_deps("libyansuan")
+    add_deps("libyansuan-static")
     add_files("src/main.cpp")
     add_platform_specific_flags()
