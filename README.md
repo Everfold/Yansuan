65b43ba1ff4a3b2391fd3f9d2697a5b05926c6c3
Hobr <mail@hobr.site>
2026-09-10 19:03:38 +0800

    elixir: env init

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Beamlet
+
+> Light on device, smart on the mesh.
+
+***WIP***
+
+## Build
+
+```bash
+# Development shell
+nix develop
+
+# Package
+mix local.hex
+mix local.rebar
+```
+
+## Thanks
+
+- [Elixir](https://elixir-lang.org/)
+- [Nix](https://nixos.org/)
--- flake.nix
+++ flake.nix
@@ -3,13 +3,39 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flakelight.url = "github:nix-community/flakelight";
   };
+
   outputs =
     { flakelight, ... }@inputs:
     flakelight ./. {
       inherit inputs;
-      devShell = pkgs: {
-        packages = with pkgs; [
-        ];
-      };
+
+      devShell =
+        pkgs:
+        let
+          otpVersion = "29";
+          elixirVersion = "1_20";
+          beam = pkgs."beamMinimal${otpVersion}Packages";
+          elixir = beam."elixir_${elixirVersion}";
+        in
+        {
+          packages =
+            (with pkgs; [
+            ])
+            ++ (with beam; [
+              elixir
+              erlang
+              expert
+            ]);
+
+          shellHook = ''
+            export MIX_HOME="$PWD/.nix-mix"
+            export HEX_HOME="$PWD/.nix-hex"
+            export MIX_ARCHIVES="$MIX_HOME/archives"
+            export PATH="$MIX_HOME/bin:$PATH"
+
+            mkdir -p "$MIX_HOME" "$HEX_HOME"
+            export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
+          '';
+        };
     };
 }
